5d7d5513dc9565233ceb9247c01ea290d11e156e
Gustavo de Vito <gustavodevito38@gmail.com>
2023-09-15 12:36:46 -0300

    .

--- src/app/image-preview/page.tsx
+++ src/app/image-preview/page.tsx
@@ -83,7 +83,10 @@ export default function ImagePreview() {
           )}
           <div className="flex items-center justify-center">
             <div className="w-full">
-              <button className="w-full text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 flex items-center justify-center mr-2 mb-2 cursor-pointer" onClick={() => console.log("Upload")}>
+              <button
+                className="w-full text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 flex items-center justify-center mr-2 mb-2 cursor-pointer"
+                onClick={() => console.log("Upload")}
+              >
                 <span className="text-center ml-2">Upload</span>
               </button>
             </div>
